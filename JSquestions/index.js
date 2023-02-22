7c9d9c64ef13b6b4fdadf6bdffd7ed6866b626f2
ALIA <alivamallick9@gmail.com>
2023-02-22 09:39:58 +0530

    map vs foreach concept added

--- JSquestions/index.js
+++ JSquestions/index.js
@@ -1,3 +1,13 @@
 // Question 1: map vs forEach
 
 const arr = [5, 7, 9, 8, 6];
+const mapResult = arr
+  ?.map((item) => {
+    return item + 2;
+  })
+  .filter((item) => item > 7);
+
+const forEachResult = arr?.forEach((ar, i) => {
+  ar[i] = ar + 3;
+});
+console.log(mapResult, forEachResult, arr);
