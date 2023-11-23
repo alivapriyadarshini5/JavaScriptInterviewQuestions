7ddba206ff554276737a401acebad5168cda9357
ALIA <alivamallick9@gmail.com>
2023-11-23 20:58:47 +0530

    what is js and role of js

--- JSquestions/index.js
+++ JSquestions/index.js
@@ -9,7 +9,7 @@ var a;
 console.log(a); //o/p:- null
 console.log(typeof a); //o/p:- object
 
-// Question 1: map vs forEach ❓
+// Question 2: map vs forEach ❓
 const arr = [5, 7, 9, 8, 6];
 const mapResult = arr
   ?.map((item) => {
@@ -21,3 +21,7 @@ const forEachResult = arr?.forEach((ar, i) => {
   arr[i] = ar + 3;
 });
 // console.log(mapResult, forEachResult, arr);
+
+//Question 3: What is JavaScript ? What is the role of JavaScript engine?
+//JavaScript is a programming language that is used for converting static web pages to interactive and dynamic web pages.
+//A JS engine is a program present in web browsers that executes in JS code.
