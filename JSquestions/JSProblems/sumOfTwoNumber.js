db70c81cb686b0cc5a910a8bf38e9b16d89814ef
ALIA <alivamallick9@gmail.com>
2023-11-25 20:59:45 +0530

    Sum of two numbers

---
+++ JSquestions/JSProblems/sumOfTwoNumber.js
@@ -0,0 +1,15 @@
+//Write a JS function to calcullate the sum of two numbers ?
+const sumOfTwoNum=(a,b)=>{
+    return a+b
+}
+const sum = sumOfTwoNum(2,3)  //o/p-5
+// const sum = sumOfTwoNum(2,"3")  o/p-23
+// const sum = sumOfTwoNum("2",3) o/p-23
+// const sum = sumOfTwoNum("2","3") o/p-23
+// const sum = sumOfTwoNum(undefined,2) o/p-NaN
+// const sum = sumOfTwoNum(2) o/p-NaN
+
+
+
+
+console.log(sum)
