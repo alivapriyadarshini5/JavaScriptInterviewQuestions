74056d8d60969475061511755cf983865c621b30
ALIA <alivamallick9@gmail.com>
2023-11-26 20:29:42 +0530

    reverse string

---
+++ JSquestions/JSProblems/reverseString.js
@@ -0,0 +1,5 @@
+//Write a JavaScript program to reverse a given string.
+const reverseString = (str) => {
+  return str.split("").reverse().join("");
+};
+console.log(reverseString("sdh")); //o/p:-hds
