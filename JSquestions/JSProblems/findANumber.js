c61a41f8f21785b6a9f65d51e6bace9e0cb77b3e
Aliva Priyadarshini <69763236+alivapriyadarshini5@users.noreply.github.com>
2024-01-23 10:02:22 +0530

    find a number

---
+++ JSquestions/JSProblems/findANumber.js
@@ -0,0 +1,6 @@
+//Find a number in Array 
+const findNumberInArray = (a, arr) => {
+    const finalValue = arr?.filter((num) => num === a);
+    return finalValue?.length ? finalValue?.length : 0;
+  };
+  // console.log(findNumberInArray(9, [5, 5, 7, 8]));
