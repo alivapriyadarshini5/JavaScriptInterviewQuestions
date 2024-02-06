a7429736d2cb123f592f9a9d5cc71711dc64f38e
Aliva Priyadarshini <69763236+alivapriyadarshini5@users.noreply.github.com>
2024-02-06 16:30:29 +0530

    convert to array

---
+++ JSquestions/JSProblems/convertToArray.js
@@ -0,0 +1,10 @@
+//In JavaScript, how do you turn an Object into an Array []?
+
+const obj = {
+    a: 1,
+    b: 2,
+  };
+  
+  console.log(Object.keys(obj));
+  console.log(Object.entries(obj));
+  console.log(Object.entries(obj));
